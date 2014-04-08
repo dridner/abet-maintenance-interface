009832a0534664d3eb2214cbdfc09076a6d8a616
David Ridner <david.ridner@gmail.com>
2014-04-07 22:20:02 -0400

    Fixed the RegisterModel, and added the registration pages.

--- AMI.MVC.WebApp/Models/Account/RegisterModel.cs
+++ AMI.MVC.WebApp/Models/Account/RegisterModel.cs
@@ -9,16 +9,20 @@ namespace AMI.MVC.WebApp.Models.Account
     public class RegisterModel
     {
         [Required]
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
         [Required]
+        [Display(Name = "Confirm Email Address")]
         [Compare("EmailAddress", ErrorMessage = "Email Addresses do not match.")]
         public string ConfirmEmailAddress { get; set; }
 
         [Required]
+        [Display(Name = "Enter Password")]
         public string Password { get; set; }
 
         [Required]
+        [Display(Name = "Confirm Password")]
         [Compare("ConfirmPassword", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
