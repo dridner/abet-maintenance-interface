df07056b4c7e9c60e1bbf746d284bd53f6cd7052
LucaFox <knightdfwm@yahoo.com>
2014-04-16 18:04:13 -0400

    committing more of the class edit change

---
+++ AMI.MVC.WebApp/Models/SLO/SLOModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMI.MVC.WebApp.Models.SLO
+{
+    public class SLOModel
+    {
+        public int ID { get; set; }
+        public int ClassID { get; set; }
+        public string Text { get; set; }
+        public List<int> SupportedOutcomeIDs { get; set; }
+    }
+}
