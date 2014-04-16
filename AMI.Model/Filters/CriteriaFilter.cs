740a3192b532c02a96a725df96be4953de90aa92
David Ridner <david.ridner@gmail.com>
2014-04-16 18:03:52 -0400

    Modified the data model.

---
+++ AMI.Model/Filters/CriteriaFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMI.Model.Filters
+{
+    public class CriteriaFilter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
