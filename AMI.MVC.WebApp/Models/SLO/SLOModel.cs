7f0369a81f4fcbbf2f0c87f9e34a4fd62e33c001
LucaFox <knightdfwm@yahoo.com>
2014-04-16 20:12:06 -0400

    bugfix

--- AMI.MVC.WebApp/Models/SLO/SLOModel.cs
+++ AMI.MVC.WebApp/Models/SLO/SLOModel.cs
@@ -5,6 +5,11 @@ namespace AMI.MVC.WebApp.Models.SLO
 {
     public class SLOModel
     {
+        public SLOModel()
+        {
+            this.SupportedOutcomes = new List<Outcome>();
+        }
+
         public int ID { get; set; }
         public int ClassID { get; set; }
         public string Text { get; set; }
