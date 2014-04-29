6c590fc73c03faaa16b6cd239047c9f05a96c476
David Ridner <david.ridner@gmail.com>
2014-04-28 21:37:23 -0400

    Added user list screen.

---
+++ AMI.Business/Logic/UserLogic/GetUserListCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AMI.Data.DataConnection;
+using AMI.Model;
+using AMI.Model.Filters;
+using System.Data.Entity;
+
+namespace AMI.Business.Logic.UserLogic
+{
+    public class GetUserListCommand : AsyncDBCommandBase<List<ApplicationUser>>
+    {
+        public delegate GetUserListCommand Factory(UserFilter filter);
+
+        private UserFilter _filter;
+
+        public GetUserListCommand(UserFilter filter)
+        {
+            this._filter = filter;
+        }
+
+        internal async override Task<List<ApplicationUser>> Execute(IDBConnection conn)
+        {
+            IQueryable<ApplicationUser> queryable = conn.UserManager.Users;
+            queryable = queryable.Where(m => m.FirstName != "SYSTEM");//Never send down the system user.
+
+            if (this._filter != null)
+            {
+                //TODO
+            }
+
+            queryable = queryable.OrderBy(m => m.FirstName);
+
+            return await queryable.ToListAsync();
+        }
+    }
+}
--- AMI.MVC.WebApp/Areas/Admin/Controllers/UserController.cs
+++ AMI.MVC.WebApp/Areas/Admin/Controllers/UserController.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using AMI.Business.Logic.UserLogic;
+using AMI.Model.Filters;
 
 namespace AMI.MVC.WebApp.Areas.Admin.Controllers
 {
     [Authorize(Roles = "SiteAdmin")]
     public partial class UserController : Controller
     {
-        //
-        // GET: /Admin/User/
-        public virtual ActionResult Index()
+        private GetUserListCommand.Factory _getUserListCommand;
+
+        public UserController(GetUserListCommand.Factory getUserListCommand)
         {
-            return View();
+            this._getUserListCommand = getUserListCommand;
+        }
+
+        [HttpGet]
+        public async virtual Task<ActionResult> Index()
+        {
+            return View(await this._getUserListCommand(null).Execute());
         }
 	}
 }
